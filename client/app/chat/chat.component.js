8101c5afda45bf495df9bd81bf72792de7c21c5b
sani <khursani8@gmail.com>
2017-01-29 01:34:10 +0800

    chat design v0.1

--- client/app/chat/chat.component.js
+++ client/app/chat/chat.component.js
@@ -26,7 +26,10 @@ export class ChatComponent {
           this.chats = response.data;
           this.socket.syncUpdates('chat', this.chats);
         });
-    }
+      // this.chats = [{"_id":"588b963e32d86075c236ca82","name":"Sani","message":"Test from robomongo","active":true},{"_id":"588c3295d2aece673e0157fa","name":"Khursani","message":"ada","__v":0},{"_id":"588c3baed2aece673e0157fe","name":"Khursani","message":"lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum","__v":0},{"_id":"588c7540d2aece673e015800","name":"user1","message":"hai waawaawdawda","__v":0}]
+      // this.socket.syncUpdates('chat', this.chats);
+  
+}
 
     addChat() {
       console.log('Add chat :',this.name);
